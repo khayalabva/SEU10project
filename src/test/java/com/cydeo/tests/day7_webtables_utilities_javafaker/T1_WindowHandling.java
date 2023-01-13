09291d76616da6e656cef43fac4db59d93eba4fe
khayalabva <khayalabva@gmail.com>
2023-01-13 15:06:30 +0100

    Day08 Practices

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -3,6 +3,7 @@ package com.cydeo.tests.day7_webtables_utilities_javafaker;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -40,9 +41,17 @@ public class T1_WindowHandling {
 
             System.out.println("Current URL: " + driver.getCurrentUrl());
 
+            if(driver.getCurrentUrl().contains("etsy")){
+                break;
+            }
+
         }
 
         //5. Assert: Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
 
         //Lines to be pasted:
         //((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
